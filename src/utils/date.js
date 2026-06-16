061bdce1797298866e6597d23a2f1796ce82bcf6
SaiMaungLenn <maunglenn@gmail.com>
2026-06-16 15:25:05 +0630

    update

--- src/utils/date.js
+++ src/utils/date.js
@@ -5,9 +5,11 @@ const TZ = 'Asia/Yangon';
 const myTZ = tz(TZ);
 
 export const myanmarFormat = (date, fmt) => {
-  // Treat naive timestamps from backend as UTC before converting to Myanmar Time
-  if (typeof date === 'string' && !date.includes('Z') && !date.includes('+')) {
-    date = date.replace(' ', 'T') + '+00:00';
+  if (typeof date === 'string') {
+    date = date.replace(' ', 'T');
+    if (!date.includes('Z') && !date.includes('+')) {
+      date += '+00:00';
+    }
   }
   return format(date, fmt, { in: myTZ });
 };
