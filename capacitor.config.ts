9d2500df6d7d6f6cb246b456765cbe2581503f00
SaiMaungLenn <maunglenn@gmail.com>
2026-06-19 11:09:51 +0630

    Initial commit with Capacitor Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.teleshop.admin',
+  appName: 'TeleshopAdmin',
+  webDir: 'dist'
+};
+
+export default config;
