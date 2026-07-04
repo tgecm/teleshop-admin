0540808555e56324f9de90d6dd1058aa5160f9b9
SaiMaungLenn <maunglenn@gmail.com>
2026-07-04 23:25:41 +0630

    auto: update

--- src/components/layout/PublicLayout.jsx
+++ src/components/layout/PublicLayout.jsx
@@ -1,11 +1,3 @@
-import PullToRefresh from '../shared/PullToRefresh';
-
 export default function PublicLayout({ children }) {
-  return (
-    <PullToRefresh onRefresh={async () => {
-      window.location.href = window.location.pathname + '?_hc=' + Date.now();
-    }}>
-      {children}
-    </PullToRefresh>
-  );
+  return <>{children}</>;
 }
