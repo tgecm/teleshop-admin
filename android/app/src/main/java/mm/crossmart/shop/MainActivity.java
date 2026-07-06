4b5e228856b263e2fb72b3f35bc0358b07d868da
SaiMaungLenn <maunglenn@gmail.com>
2026-07-06 10:48:40 +0630

    auto: update

--- android/app/src/main/java/mm/crossmart/shop/MainActivity.java
+++ android/app/src/main/java/mm/crossmart/shop/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ecommercemyanmar.shop;
+package mm.crossmart.shop;
 
 import android.app.DownloadManager;
 import android.content.ContentValues;
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ecommercemyanmar.shop',
-  appName: 'E-commerce',
+  appId: 'mm.crossmart.shop',
+  appName: 'CrossMart',
   webDir: 'dist',
   server: {
     androidScheme: 'http',
